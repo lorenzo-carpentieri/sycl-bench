98ea4cf5817964468a4d1170a5267f3749809fe6
emdant <30555419+emdant@users.noreply.github.com>
2023-03-02 16:29:36 +0100

    Adjustments to benchmarks

--- micro-bench-energy/LocalMemory.cpp
+++ micro-bench-energy/LocalMemory.cpp
@@ -1,47 +1,48 @@
-#include <iostream>
 #include "common.h"
+#include <iostream>
 
 using namespace sycl;
 
 
 template <typename ValueType, size_t LocalSize>
-class LocalMemory{
-  protected:
-    size_t size;
-    size_t iters;
-    BenchmarkArgs& args;
-  public:
-    LocalMemory(BenchmarkArgs& _args):args(_args){}
+class LocalMemory {
+protected:
+  size_t size;
+  size_t iters;
+  BenchmarkArgs& args;
 
-    void setup(){
-      size = args.problem_size;
-      iters = args.num_iterations;
-    }
+public:
+  LocalMemory(BenchmarkArgs& _args) : args(_args) {}
 
-    void run(std::vector<sycl::event>& events){
-      // Launch the computation
-      sycl::event e = args.device_queue.submit([&](handler& h) {
-        sycl::local_accessor<ValueType, 1> local_acc1{LocalSize, h};
+  void setup() {
+    size = args.problem_size;
+    iters = args.num_iterations;
+  }
 
-        sycl::nd_range<1> ndr{size, LocalSize};
-        h.parallel_for(ndr, [=, _size=size, compute_iters=iters](sycl::nd_item<1> item) {
-          int gid = item.get_global_id(0);
-          int lid = item.get_local_id(0);
-          ValueType r0;
+  void run(std::vector<sycl::event>& events) {
+    // Launch the computation
+    sycl::event e = args.device_queue.submit([&](handler& h) {
+      sycl::local_accessor<ValueType, 1> local_acc1{LocalSize, h};
 
-          if (gid < _size) {
-            for (int i = 0; i < compute_iters; i++) {
-              r0 = local_acc1[lid];
-              local_acc1[LocalSize - lid - 1] = r0;
-            }
+      sycl::nd_range<1> ndr{size, LocalSize};
+      h.parallel_for(ndr, [=, _size = size, compute_iters = iters](sycl::nd_item<1> item) {
+        int gid = item.get_global_id(0);
+        int lid = item.get_local_id(0);
+        ValueType r0;
+
+        if(gid < _size) {
+          for(int i = 0; i < compute_iters; i++) {
+            r0 = local_acc1[lid];
+            local_acc1[LocalSize - lid - 1] = r0;
           }
-        }); //end parallel_for
-      });   //end submit
-      events.push_back(e);
-    }
+        }
+      }); // end parallel_for
+    });   // end submit
+    events.push_back(e);
+  }
 
 
-    static std::string getBenchmarkName() {
+  static std::string getBenchmarkName() {
     std::string name = "LocalMemory_";
     name.append(std::is_same_v<ValueType, int> ? "int" : "float");
     return name;
@@ -50,9 +51,7 @@ class LocalMemory{
   bool verify(VerificationSetting& ver) { return true; }
 };
 
-
-
-// 1000000 131072
+// 1000000 1000000
 int main(int argc, char** argv) {
   BenchmarkApp app(argc, argv);
   app.run<LocalMemory<float, 32>>();
