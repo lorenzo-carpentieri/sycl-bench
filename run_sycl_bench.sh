69f681dcc9f85403c992046f5acdfd729d5d667f
lorenzo-carpentieri <lorenzocarpentieri34@gmail.com>
2024-02-02 17:38:52 +0000

    Updated run_sycl_bench

--- run_sycl_bench.sh
+++ run_sycl_bench.sh
@@ -76,7 +76,6 @@ for core_freq in $core_frequencies; do
   fi
   sampled_freq+=($core_freq)
 done
-
 mem_freq=$def_mem
 for core_freq in $core_frequencies; do
   # For the amd platform and Intel gpu platform we consider the default frequency the auto mode of the GPUs
@@ -86,41 +85,41 @@ for core_freq in $core_frequencies; do
     rocm-smi --device=0 --setperflevel manual > /dev/null
   fi
   echo "[*] Running benchmarks for frequency $core_freq"
-  # $SCRIPT_DIR/build/bit_compression --device=gpu --size=131072 --num-iters=1000000 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/bit_compression_${mem_freq}_${core_freq}.log
-  # echo -n "* "
-  # $SCRIPT_DIR/build/black_scholes --device=gpu --size=131072 --num-iters=50000 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/black_scholes_${mem_freq}_${core_freq}.log
-  # echo -n "* "
-  # $SCRIPT_DIR/build/box_blur --device=gpu --size=1024 --num-iters=100 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/box_blur_${mem_freq}_${core_freq}.log
-  # echo -n "* "
-  # $SCRIPT_DIR/build/ftle --device=gpu --size=262144 --num-iters=500000 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/ftle_${mem_freq}_${core_freq}.log
-  # echo -n "* "
-  # $SCRIPT_DIR/build/geometric_mean --device=gpu --size=16384 --num-iters=20000 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/geometric_mean_${mem_freq}_${core_freq}.log
-  # echo -n "* "
-  # $SCRIPT_DIR/build/kmeans --device=gpu --size=32768 --num-iters=50000 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/kmeans_${mem_freq}_${core_freq}.log
-  # echo -n "* "
-  # $SCRIPT_DIR/build/knn --device=gpu --size=8192 --num-iters=15 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/knn_${mem_freq}_${core_freq}.log
+  $SCRIPT_DIR/build/bit_compression --device=gpu --size=131072 --num-iters=1000000 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/bit_compression_${mem_freq}_${core_freq}.log
   echo -n "* "
-  $SCRIPT_DIR/build/lin_reg_error --device=gpu --num-iters=2000 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/lin_reg_error_${mem_freq}_${core_freq}.log
-  # echo -n "* "
-  # $SCRIPT_DIR/build/matrix_mul --device=gpu --size=2048 --num-iters=50 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/matrix_mul_${mem_freq}_${core_freq}.log
-  # echo -n "* "
-  # $SCRIPT_DIR/build/matrix_transpose_local_mem --device=gpu --size=4096 --num-iters=2000 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/matrix_transpose_local_mem_${mem_freq}_${core_freq}.log
+  $SCRIPT_DIR/build/black_scholes --device=gpu --size=131072 --num-iters=50000 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/black_scholes_${mem_freq}_${core_freq}.log
   echo -n "* "
-  $SCRIPT_DIR/build/median --device=gpu --num-iters=2000 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/median_${mem_freq}_${core_freq}.log
-  # echo -n "* "
-  # $SCRIPT_DIR/build/merse_twister --device=gpu --size=262144 --num-iters=50000 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/merse_twister_${mem_freq}_${core_freq}.log
-  # echo -n "* "
-  # $SCRIPT_DIR/build/mol_dyn  --device=gpu --size=60000 --num-iters=200000 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/mol_dyn_${mem_freq}_${core_freq}.log
-  # echo -n "* "
-  # $SCRIPT_DIR/build/nbody_local_mem --device=gpu --size=8192 --num-iters=500 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/nbody_local_mem_${mem_freq}_${core_freq}.log
-  # echo -n "* "
-  # $SCRIPT_DIR/build/scalar_prod --device=gpu --size=6291456 --num-iters=20000 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/scalar_prod_${mem_freq}_${core_freq}.log
-  # echo -n "* "
-  # $SCRIPT_DIR/build/sinewave --device=gpu --size=8192 --num-iters=10000 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/sinewave_${mem_freq}_${core_freq}.log
-  # echo -n "* "
-  # $SCRIPT_DIR/build/sobel  --device=gpu --size=1024 --num-iters=50 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/sobel_${mem_freq}_${core_freq}.log
-  # echo "*"
-  # $SCRIPT_DIR/build/vec_add  --device=gpu --size=1048576 --num-iters=100000 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/vec_add_${mem_freq}_${core_freq}.log
+  $SCRIPT_DIR/build/box_blur --device=gpu --size=1024 --num-iters=100 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/box_blur_${mem_freq}_${core_freq}.log
+  echo -n "* "
+  $SCRIPT_DIR/build/ftle --device=gpu --size=262144 --num-iters=500000 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/ftle_${mem_freq}_${core_freq}.log
+  echo -n "* "
+  $SCRIPT_DIR/build/geometric_mean --device=gpu --size=16384 --num-iters=20000 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/geometric_mean_${mem_freq}_${core_freq}.log
+  echo -n "* "
+  $SCRIPT_DIR/build/kmeans --device=gpu --size=32768 --num-iters=50000 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/kmeans_${mem_freq}_${core_freq}.log
+  echo -n "* "
+  $SCRIPT_DIR/build/knn --device=gpu --size=8192 --num-iters=15 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/knn_${mem_freq}_${core_freq}.log
+  echo -n "* "
+  $SCRIPT_DIR/build/lin_reg_error --device=gpu --num-iters=2000 --num-runs=$runs  --core-freq=${core_freq} > $SCRIPT_DIR/logs/lin_reg_error_${mem_freq}_${core_freq}.log
+  echo -n "* "
+  $SCRIPT_DIR/build/matrix_mul --device=gpu --size=5000 --num-iters=1 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/matrix_mul_${mem_freq}_${core_freq}.log
+  echo -n "* "
+  $SCRIPT_DIR/build/matrix_transpose_local_mem --device=gpu --size=4096 --num-iters=2000 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/matrix_transpose_local_mem_${mem_freq}_${core_freq}.log
+  echo -n "* "
+  $SCRIPT_DIR/build/median --device=gpu --num-iters=2000 --num-runs=$runs  --core-freq=${core_freq} > $SCRIPT_DIR/logs/median_${mem_freq}_${core_freq}.log
+  echo -n "* "
+  $SCRIPT_DIR/build/merse_twister --device=gpu --size=524288 --num-iters=50000  --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/merse_twister_${mem_freq}_${core_freq}.log
+  echo -n "* "
+  $SCRIPT_DIR/build/mol_dyn  --device=gpu --size=60000 --num-iters=200000 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/mol_dyn_${mem_freq}_${core_freq}.log
+  echo -n "* "
+  $SCRIPT_DIR/build/nbody_local_mem --device=gpu --size=8192 --num-iters=500 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/nbody_local_mem_${mem_freq}_${core_freq}.log
+  echo -n "* "
+  $SCRIPT_DIR/build/scalar_prod --device=gpu --size=6291456 --num-iters=20000 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/scalar_prod_${mem_freq}_${core_freq}.log
+  echo -n "* "
+  $SCRIPT_DIR/build/sinewave --device=gpu --size=8192 --num-iters=10000 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/sinewave_${mem_freq}_${core_freq}.log
+  echo -n "* "
+  $SCRIPT_DIR/build/sobel  --device=gpu --size=1024 --num-iters=50 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/sobel_${mem_freq}_${core_freq}.log
+  echo "*"
+  $SCRIPT_DIR/build/vec_add  --device=gpu --size=1048576 --num-iters=100000 --num-runs=$runs --memory-freq=${mem_freq} --core-freq=${core_freq} > $SCRIPT_DIR/logs/vec_add_${mem_freq}_${core_freq}.log
 done
 
 # Reset default configuration
