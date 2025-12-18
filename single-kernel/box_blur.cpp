81ca4cc8319fc0aa75fa6d25725617b834601673
lorenzo-carpentieri <lorenzocarpentieri34@gmail.com>
2025-12-18 14:02:25 +0000

    Replaced sycl.hpp include with sycl/sycl.hpp

--- single-kernel/box_blur.cpp
+++ single-kernel/box_blur.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <sycl.hpp>
+#include <sycl/sycl.hpp>
 #include "bitmap.h"
 
 #include "common.h"
--- single-kernel/perlin.cpp
+++ single-kernel/perlin.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <sycl.hpp>
+#include <sycl/sycl.hpp>
 
 #include "common.h"
 
